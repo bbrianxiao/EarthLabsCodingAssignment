2e3fd9d3faa9c06b4b17fc30ad1afad4553f103e
Brian Xiao <brianxiao@v1041-wn-rt-a-195-238.campus-dynamic.uwaterloo.ca>
2023-02-01 00:44:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import {decode} from "html-entities";
 
 function App() {
+  const [bpi, setBpi] = useState({});
+  const [filteredBpi, setFilteredBpi] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [fetchTime, setFetchTime] = useState("");
+  const [updatedTime, setUpdatedTime] = useState("");
+
+  const fetchData = async () => {
+    setLoading(true);
+    const res = await axios.get(
+      "https://api.coindesk.com/v1/bpi/currentprice.json"
+    );
+    console.log(res);
+    setBpi(res.data.bpi);
+    setFilteredBpi(res.data.bpi);
+    setLoading(false);
+    const fetchTime = new Date().toLocaleString();
+    setFetchTime(fetchTime);
+    setLoading(false);
+    setUpdatedTime(res.data.time.updated);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleFilter = (e) => {
+    const filterValue = e.target.value.toLowerCase();
+    setFilteredBpi(
+      Object.keys(bpi)
+        .filter((currency) => currency.toLowerCase().includes(filterValue))
+        .reduce((acc, cur) => ({ ...acc, [cur]: bpi[cur] }), {})
+    );
+  };
+
+  const handleSort = (e) => {
+    setFilteredBpi(
+      Object.keys(filteredBpi)
+        .sort()
+        .reduce((acc, cur) => ({ ...acc, [cur]: filteredBpi[cur] }), {})
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading && <p>Loading...</p>}
+      {!loading && (
+        <>
+        <h1>Currency Rate Exchanger Created By Brian Xiao</h1>
+          <input
+            type="text"
+            placeholder="Filter by code"
+            onChange={handleFilter}
+          />
+          <button id="sortbtn" onClick={handleSort}>Sort</button>
+          <table>
+            <thead>
+              <tr>
+                <th>Code</th>
+                <th>Symbol</th>
+                <th>Rate</th>
+                <th>Description</th>
+                <th>Rate Float</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(filteredBpi).map((currency) => (
+                <tr key={currency}>
+                  <td>{currency}</td>
+                  <td>{decode(filteredBpi[currency].symbol)}</td>
+                  <td>{filteredBpi[currency].rate}</td>
+                  <td>{filteredBpi[currency].description}</td>
+                  <td>{filteredBpi[currency].rate_float}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <p>Data Last Updated at: {updatedTime}</p>
+          <p>Last Refreshed at: {fetchTime}</p>
+
+          <button id="getdatabtn" onClick={fetchData}>Refresh Data</button>
+        </>
+      )}
     </div>
   );
 }
